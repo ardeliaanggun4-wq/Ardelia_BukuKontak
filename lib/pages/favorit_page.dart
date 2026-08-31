fca59503c9a2a9625c69e31a189477812d08b6f8
Ardelia Anggun Saputri <ardeliaanggun4@gmail.com>
2026-08-31 10:11:04 +0700

    Buku Kontak

---
+++ lib/pages/favorit_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavoritPage extends StatelessWidget {
+  const FavoritPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Belum ada kontak favorit.',
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+}
