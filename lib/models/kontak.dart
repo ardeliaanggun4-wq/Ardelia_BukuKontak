057705abd35f94a7bbb8bc69d7613b260b073a3a
ardeliaanggun4-wq <ardeliaanggun4@gmail.com>
2026-09-07 10:28:16 +0700

    Menambah Kategori Kontak

--- lib/models/kontak.dart
+++ lib/models/kontak.dart
@@ -2,10 +2,12 @@ class Kontak {
   final String nama;
   final String email;
   final String noHp;
+  final String? kategori;
 
   Kontak({
     required this.nama,
     required this.email,
     required this.noHp,
+    this.kategori,
   });
 }
--- lib/pages/kontak_page.dart
+++ lib/pages/kontak_page.dart
@@ -28,7 +28,7 @@ class KontakPage extends StatelessWidget {
               ),
             ),
             title: Text(kontak.nama),
-            subtitle: Text('${kontak.email}\n${kontak.noHp}'),
+            subtitle: Text('${kontak.email}\n${kontak.noHp}\n${kontak.kategori ?? "Tanpa kategori"}'),
             isThreeLine: true,
           ),
         );
